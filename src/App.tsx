53e7dc187a2ac26b7b8c5aeed3d3f42772a33688
H4KB <akabaharuka@gmail.com>
2022-04-23 13:30:08 +0900

    feat: create gird layout of button.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./img/walking.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="walking" />
+        <div>Walking BINGO</div>
       </header>
+      <body>
+        <div id="bingo">
+          <div className="grid-container">
+            {Array(9)
+              .fill(0)
+              .map((_, i) => (
+                <div className="grid-item">
+                  <button key={i} className="square">
+                    hello
+                  </button>
+                </div>
+              ))}
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
